7675c2c2d782629161b7dd4d8fec53f7c2261617
Mite Mitreski <mmitreski@tricode.nl>
2015-01-25 02:11:43 +0100

    more problem solutions

---
+++ projectEuler/projectEuler/src/main/java/Problem34.java
@@ -0,0 +1,39 @@
+import com.google.common.collect.Maps;
+
+import java.util.Map;
+
+/**
+ * Digit factorials.
+ */
+public class Problem34 {
+
+  public static void main(String[] args) {
+    Map<Long, Long> digitsFactMap = Maps.newHashMap();
+    digitsFactMap.put(0L, EulerUtil.factorial(0));
+    digitsFactMap.put(1L, EulerUtil.factorial(1));
+    digitsFactMap.put(2L, EulerUtil.factorial(2));
+    digitsFactMap.put(3L, EulerUtil.factorial(3));
+    digitsFactMap.put(4L, EulerUtil.factorial(4));
+    digitsFactMap.put(5L, EulerUtil.factorial(5));
+    digitsFactMap.put(6L, EulerUtil.factorial(6));
+    digitsFactMap.put(7L, EulerUtil.factorial(7));
+    digitsFactMap.put(8L, EulerUtil.factorial(8));
+    digitsFactMap.put(9L, EulerUtil.factorial(9));
+
+    long total = 0;
+    for (long i = 9; i < 1000000; i++) {
+      long sum = 0;
+      long num = i;
+      while (num != 0) {
+        long digit = num % 10;
+        num = num / 10;
+        sum += digitsFactMap.get(digit);
+      }
+      if (sum == i) {
+        total += i;
+        System.out.println("--" + i);
+      }
+    }
+    System.out.println("total -  " + total);
+  }
+}
---
+++ projectEuler/projectEuler/src/test/java/EulerUtilTest.java
@@ -0,0 +1,28 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class EulerUtilTest {
+
+  @Test
+  public void testIsPalindrome() throws Exception {
+
+    assertTrue(EulerUtil.isPalindrome("ama"));
+    assertTrue(EulerUtil.isPalindrome("10101"));
+
+  }
+
+  @Test
+  public void isNotPaling() throws Exception {
+
+    assertFalse(EulerUtil.isPalindrome("mite"));
+
+  }
+
+  @Test
+  public void permuteTest() {
+    System.out.println(EulerUtil.permutation("ABC"));
+
+  }
+}
