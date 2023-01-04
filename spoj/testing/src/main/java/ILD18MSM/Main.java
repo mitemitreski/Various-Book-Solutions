cd54bf6bb52acef6a6856098097d61c674d487b0
Mite Mitreski <mite.mitreski@gmail.com>
2023-01-04 22:17:00 +0100

    started a new problem

---
+++ spoj/testing/src/main/java/ILD18MSM/Main.java
@@ -0,0 +1,85 @@
+package ILD18MSM;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+
+/*
+
+Input:
+aab
+baa
+
+Output:
+1
+
+Input:
+aaaaabaaa
+aaaaaabaa
+
+Output:
+1
+
+
+Input:
+aabaaaaa
+aaaaaabaa
+
+Output:
+1
+
+
+Input:
+baaaaaaa
+aaaaaaab
+
+Output:
+1
+
+
+Input:
+baaaaaaa
+aaaaaaba
+
+Output:
+2
+ */
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+        String a = r.readLine();
+        String b = r.readLine();
+
+
+        if (a.equals(b)) {
+            System.out.println(0);
+            return;
+        }
+
+        int output = caluclateNumbeOfMoves(a, b);
+
+        System.out.println(output);
+    }
+
+    public static int caluclateNumbeOfMoves(String a, String b) {
+        int numberOfMoves = 0;
+        for (int i = 0; i < a.length(); i++) {
+            int j = i;
+            int countMoves = 0;
+            while (j < b.length() && a.charAt(i) != b.charAt(j)) {
+                j++;
+                countMoves++;
+            }
+            if (countMoves > a.length() / 2) {
+                countMoves = a.length() / 2;
+            }
+            numberOfMoves += countMoves;
+
+        }
+        throw new RuntimeException("not solved");
+//        int output = numberOfMoves / 2;
+//        return output;
+
+    }
+}
