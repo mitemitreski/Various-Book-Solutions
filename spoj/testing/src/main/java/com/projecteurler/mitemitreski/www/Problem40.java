f1155ecd4ce4cc306e4109987af43ad722fda434
Mite Mitreski <mite.mitreski@gmail.com>
2023-01-15 00:40:38 +0100

    added problem 40

---
+++ spoj/testing/src/main/java/com/projecteurler/mitemitreski/www/Problem40.java
@@ -0,0 +1,35 @@
+package com.projecteurler.mitemitreski.www;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+
+public class Problem40 {
+
+
+    public static void main(String[] args) throws IOException {
+        StringBuilder out = new StringBuilder("");
+        for (int i = 0; i < 1000000; i++) {
+            out.append(i);
+        }
+
+
+        int digitis = 12;
+
+        System.out.println("test example " + out.charAt(digitis));
+        //d1 × d10 × d100 × d1000 × d10000 × d100000 × d1000000
+        int[] indexes = {1, 10, 100, 1000, 10000, 100000, 1000000};
+
+        long expressionCalucalted=1L;
+        for (Integer index:indexes) {
+            expressionCalucalted*=Integer.parseInt(out.charAt(index) + "");
+        }
+
+
+        System.out.println("total is :" + expressionCalucalted);
+
+    }
+
+
+}
